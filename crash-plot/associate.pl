c52dbebdf359f5f4329b331d164e2100044a56aa
Scott Walters <scott@slowass.net>
2016-01-12 03:45:34 -0700

    make vis.R work stand-alone for plotting crash-per-count-loc
    vis.R previously relied on data generated by a query on a Postgres
    database that has a copy of bike count and crash summary data.
    that query was moved to the old/ directory in the last commit.
    the R code in vis.R now does the tallying, tabulation, aggregating,
    and averaging work needed to render the plot.
    
    this also means that it reads and parses all of the bike count
    data files, so now we have an example of that happening in R.

--- crash-plot/associate.pl
+++ crash-plot/associate.pl
@@ -4,16 +4,23 @@ use warnings;
 
 =for comments
 
+Reads the bikecrash_2*.csv files and ../count_sites.csv file, and writes bikecrash_all_years_associated.csv.
+
+bikecrash_all_years_associated.csv contains all records from all of the bikecrash_2*.csv files, with
+'nearest_count_site' and 'distance_to_nearest_count_site' fields added.
+
+'nearest_count_site' is a site ID like 123.
+'distance_to_nearest_count_site' is in units of lat/long, mixed via Pythagoras' theorum
 
 =cut
 
 use Data::Dumper;
 use IO::Handle;
+use IO::Any;
 use List::MoreUtils 'zip';
 use Cwd;
 use JSON::PP;
 use Storable;
-
 use XXX;
 use Carp;
 
@@ -21,25 +28,40 @@ use csv;
 
 # location_id,location_W_E,location_N_S,priority,vols_needed,latitude,longitude,automatic_geocoding_failed,geocoded_by,notes,2015_include,"last year done",2015_notes,Counter_notes,column_number_14
 
-my $sites = csv->new('count_sites.csv');
+my $sites = csv->new('../count_sites.csv');
 
+# combine all bikecrash_2*.csv files
+
+my $buffer = '';
+my $have_a_header = 0;
+while( my $fn = glob 'bikecrash_2*csv' ) {
+   warn "reading $fn...\n";
+   my @lines = split "\n", IO::Any->slurp($fn);
+   shift @lines if $have_a_header++;
+   $buffer .= join '', map "$_\n", @lines;
+}
+
+IO::Any->write('bikecrash_all_years_associated.csv')->print($buffer);
+
+# columns:
+#
 # IncidentID,num_vehicles_involved,eCollisionManner,Longitude,Latitude,InjurySeverity,eIntersectionType,eCollisionManner,city_name,IncidentDateTime,cyclist_citations,driver_citations
 
-my $accidents = csv->new('bikeaccident_2009_to_2013.csv');
+my $crashes = csv->new('bikecrash_all_years_associated.csv');
 
 #
 # add columns 
 #
 
 for my $column (qw/nearest_count_site distance_to_nearest_count_site/) {
-    $accidents->add_column($column) if ! grep $_ eq $column, @{ $accidents->headers };
+    $crashes->add_column($column) if ! grep $_ eq $column, @{ $crashes->headers };
 }
 
 #
 # associate
 #
 
-for my $row ( $accidents->rows ) {
+for my $row ( $crashes->rows ) {
 
     my $lon = $row->Longitude or next; # die;
     my $lat = $row->Latitude or next; # die;
@@ -71,7 +93,7 @@ for my $row ( $accidents->rows ) {
 
 }
 
-$accidents->write;
+$crashes->write;
 
 sub distance {
     my $lon1 = shift;  # longitude is X
