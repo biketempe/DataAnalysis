d904d8117465377972edcfe4ffb086b3f872f5af
Scott Walters <scott@slowass.net>
2016-01-17 11:28:59 -0700

    parametarizing plotting, fix legend position
    each plot is essentially the same, so make it into a function.
    push the legend up from the bottom left corner a bit so that
    Google's attribution isn't blocked.

--- attribute-plots/attribute_plots.R
+++ attribute-plots/attribute_plots.R
@@ -2,7 +2,7 @@ library(ggmap)
 
 # run this with eg:
 
-#  R -f attribute_plots.R --args ../raw_count_data/2014_count_data.csv
+#  R -f attribute_plots.R --args CountSummary2015_for_Geoplot.csv
 
 # plots:
 
@@ -19,40 +19,33 @@ print(count_data_fn)
 
 count_data <- read.csv(count_data_fn)[,c("Loc.ID", "Total.per.hr", "Female.", "Helmet.", "Wrong.way.", "Sidewalk.", "Accidents.per.100.hr", "latitude", "longitude")]
 
+geo_plot <- function(color, title, filename, data, field) {
+    data$data_field <- data[, field]
+    map <- get_map(location = c( lon = -111.9295916, lat = 33.4015448), zoom = 13, filename = "ggmapTemp", scale=1)
+    map <- ggmap(map) + geom_point(aes(x = longitude, y = latitude, size = data_field), data = data, colour = color)
+    map <- map + scale_size_area(name=title)
+    map <- map + theme(legend.justification=c(0,0), legend.position=c(0.02,0.02))
+    ggsave(filename=filename, plot=map)
+}
+
 # per hour
 
-map <- get_map(location = c( lon = -111.9295916, lat = 33.4015448), zoom = 13, filename = "ggmapTemp", scale=1)
-map <- ggmap(map) + geom_point(aes(x = longitude, y = latitude, size = Total.per.hr), data = count_data, colour = 'black')
-map <- map + scale_size_area(name="Total Count per Hour")
-map <- map + theme(legend.justification=c(0,0), legend.position=c(0,0))
-ggsave(filename="per_hour.png", plot=map)
+geo_plot("black", "Total Count\nper Hour", "per_hour.png", count_data, "Total.per.hr")
 
 # wrong way
 
-map <- get_map(location = c( lon = -111.9295916, lat = 33.4015448), zoom = 13, filename = "ggmapTemp", scale=1)
-map <- ggmap(map) + geom_point(aes(x = longitude, y = latitude, size = Wrong.way. * 100), data = count_data, colour = 'red')
-map <- map + scale_size_area(name="Percent of Wrong Way Riders")
-map <- map + theme(legend.justification=c(0,0), legend.position=c(0,0))
-ggsave(filename="wrong_way.png", plot=map)
+geo_plot("red", "Percent of\nWrong Way Riders", "wrong_way.png", count_data, "Wrong.way.")
 
 # sidewalk
 
-map <- get_map(location = c( lon = -111.9295916, lat = 33.4015448), zoom = 13, filename = "ggmapTemp", scale=1)
-map <- ggmap(map) + geom_point(aes(x = longitude, y = latitude, size = Sidewalk. * 100), data = count_data, colour = 'purple')
-map <- map + scale_size_area(name="Percent of Riders Using Sidewalk")
-map <- map + theme(legend.justification=c(0,0), legend.position=c(0,0))
-ggsave(filename="sidewalk.png", plot=map)
+geo_plot("purple", "Percent of\nRiders Using\nSidewalk", "sidewalk.png", count_data, "Sidewalk.")
 
 # helmet
 
-map <- get_map(location = c( lon = -111.9295916, lat = 33.4015448), zoom = 13, filename = "ggmapTemp", scale=1)
-map <- ggmap(map) + geom_point(aes(x = longitude, y = latitude, size = Helmet. * 100), data = count_data, colour = 'green')
-map <- map + scale_size_area(name="Percent of Riders Wearing Helmets")
-map <- map + theme(legend.justification=c(0,0), legend.position=c(0,0))
-ggsave(filename="helmet.png", plot=map)
+geo_plot("green", "Percent of\nRiders Wearing\nHelmets", "helmet.png", count_data, "Helmet.")
 
 
-
+# code example for specifying breaks in the legend (instead of R doing it automatically):
 # map <- map +
 #    scale_size_area(breaks = sqrt(c(1, 5, 10, 50, 100, 500)), labels = c(1, 5, 10, 50, 100, 500), name = "avg total\nbikes counted\nor\ntotal collisions\n2009-2013")
 
